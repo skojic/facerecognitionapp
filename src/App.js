e785399bcc1366e024a41dbb51816e7cf1fdc1f4
skojic <srdjan.m.kojic@gmail.com>
2018-07-24 15:11:31 +0200

    Adding project files

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import './App.css';
+import 'tachyons';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        {/*
+                <ImageLinkForm />
+               <FaceRecognition />*/}
+      </div>
+    );
+  }
+}
+
+export default App;
