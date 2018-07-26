47fa2fa7e14a98649eac98be45e258003f038a91
Srdjan Kojic <srdjan.m.kojic@gmail.com>
2018-07-26 21:19:36 +0200

    adding lines of code to the App and ImageLinks files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,66 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai'
+import 'tachyons';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
-import './App.css';
-import 'tachyons';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import './App.css';  
+
+const app = new Clarifai.App({
+ apiKey: '4d59d0d99ae6487094aed4ef207a2019'
+});
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 180,
+      density: {
+        enable: true,
+        value_area: 800
+        }
+      }
+    }
+  }
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () =>{
+    console.log ('click');
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      // do something with response
+    },
+    function(err) {
+      // there was an error
+    }
+  );
+  }
   render() {
     return (
       <div className="App">
+        <Particles className='particles'
+          params={particlesOptions}
+        />
         <Navigation />
         <Logo />
-        {/*
-                <ImageLinkForm />
-               <FaceRecognition />*/}
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}
+        />
+               {/*<FaceRecognition />*/}
       </div>
     );
   }
